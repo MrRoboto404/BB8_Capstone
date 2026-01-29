4ad4c3d772e0121c3eb3f4154015a83f0f11ccd8
Ryan Stokes <spacebot05@gmail.com>
2026-01-28 22:19:14 -0800

    Adds xyz axes and support for 3d rotation

--- MATLAB Code/3D_simulation/sim_3d.m
+++ MATLAB Code/3D_simulation/sim_3d.m
@@ -8,8 +8,8 @@ clear; clc; close all;
 % Run linear model------------------------------------------------------
 
 % Construct & load params
-[A, B, C, D, ~] = make_ballbot();
-ics = [0; 0.3; 0; 0]; % initial conditions in minimal coords
+[A_yz, B_yz, C_yz, D_yz, ~] = make_ballbot();
+ics_yz = [0; -0.3; 0; 0]; % initial conditions in minimal coords
 
 
 % Input parameters into simulink model
@@ -17,11 +17,13 @@ mdl = "main_sim";
 simIn = Simulink.SimulationInput(mdl);
 
 set_param(mdl, "EnablePacing", "on"); % slow down pacing to file: 0.8x
-simIn = simIn.setVariable("A", A);
-simIn = simIn.setVariable("B", B);
-simIn = simIn.setVariable("C", C);
-simIn = simIn.setVariable("D", D);
-simIn = simIn.setVariable("ics", ics);
+simIn = setModelParameter(simIn, "StopTime", "10"); % configure run time (s)
+
+simIn = simIn.setVariable("A", A_yz);
+simIn = simIn.setVariable("B", B_yz);
+simIn = simIn.setVariable("C", C_yz);
+simIn = simIn.setVariable("D", D_yz);
+simIn = simIn.setVariable("ics_yz", ics_yz);
 
 % Run sim & vis
 out = sim(simIn);
