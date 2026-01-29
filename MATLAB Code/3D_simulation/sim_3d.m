7d58e4be64887918fc071307cc43bc017388c245
Ryan Stokes <spacebot05@gmail.com>
2026-01-28 23:17:22 -0800

    Adds output handling and theta limit in sim.

--- MATLAB Code/3D_simulation/sim_3d.m
+++ MATLAB Code/3D_simulation/sim_3d.m
@@ -17,7 +17,7 @@ mdl = "main_sim";
 simIn = Simulink.SimulationInput(mdl);
 
 set_param(mdl, "EnablePacing", "on"); % slow down pacing to file: 0.8x
-simIn = setModelParameter(simIn, "StopTime", "10"); % configure run time (s)
+simIn = setModelParameter(simIn, "StopTime", "3"); % configure run time (s)
 
 simIn = simIn.setVariable("A", A_yz);
 simIn = simIn.setVariable("B", B_yz);
@@ -27,3 +27,17 @@ simIn = simIn.setVariable("ics_yz", ics_yz);
 
 % Run sim & vis
 out = sim(simIn);
+
+% Get data
+t = out.tout;
+theta_rot = out.theta_rot; % rad
+%% MANIPULATE DATA WITHOUT RERUNNING SIM
+
+theta_x = squeeze(out.theta_rot(:, 1, :)); % rad
+theta_x = theta_x * 180/pi; % deg
+
+% plot data
+plot(t, theta_x)
+xlabel('Time (s)');
+ylabel('\theta_x, (deg)');
+
