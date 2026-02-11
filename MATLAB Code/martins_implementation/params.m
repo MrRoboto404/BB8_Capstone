d9c039cc698a705a6e3c4c75c11b5e161bff4650
Ryan Stokes <spacebot05@gmail.com>
2026-02-10 17:40:12 -0800

    Paramaterized params.m for sweeping

--- MATLAB Code/martins_implementation/params.m
+++ MATLAB Code/martins_implementation/params.m
@@ -1,48 +1,47 @@
 function p = params()
 % BALLBOT_PARAMS  Parameters for planar ballbot model (1D)
 %USE SI UNITS - starting 
-% Constants ----------------------------------------------------
-%Masses
+
+% VARIABLE properties %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%_______Masses_______
 p.m_k = 2.29;   %kg, Mass of the Ball
-p.m_w = 1000;      %kg, Mass of the virtual actuating wheel
 p.m_A = 9.2;    %kg, Mass of the body
-p.m_omni = 0.5; %kg, reverse engineered from inertia given
+p.m_omni = 4; %kg, reverse engineered from inertia given
 
-
-%Radius
+%_______Lengths_______
 p.r_k = .125;   %m, Radius of the ball
 p.r_w = .06;    %m, radius of the OMNIWHEELS, NOT VIRTUAL
-p.r_A = .1;     %m, radius of the body (Cylinder)
+p.r_A = .1;     %m, radius of the body (cylinder)
 
-% Angle of wheels on ball
-p.alpha = 45; % deg
-p.beta = 120; %deg 
+p.h_A = 0.894;  %m, height of the body (cylinder). Reverse engineered from interia calcs
+p.l = .339;     %m, Height of the center of gravity of body from center of ball
 
-% Body distance
-p.l = .339;     %m, Height of the center of gravity
+%_______Angle of wheels on ball_______
+p.alpha = deg2rad(45); %rad
+p.beta = deg2rad(30); %rad, taken from +X axis (adds intervals for other locations)
 
-% Gravity
-p.g = 9.81;               % m/s^2, Gravitational acceleration
+%_______Misc_______
+p.g = 9.81;     % m/s^2, Gravitational acceleration
+p.i_Gear = 26;  % -, Gear ratio
 
-% Gear train
-p.i_Gear = 26;            % -, Gear ratio
-
-% Inertias
+%_______Inertia Constants_______
 p.Theta_m = 3.33 * 10^-6; % rotor intertia (real small)
-p.Theta_A    = 4.76;      % kg*m^2, Inertia of the body
-p.Theta_A_xy = 0.092;     % kg*m^2, Inertia of the body in xy-plane
 
-% Calculations ------------------------------------------------------
-% Dims
+% CALCULATIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Inertias
+% _________Inertias___________
 p.Theta_k = 2/3 * p.m_k * p.r_k^2; %kgm^2 Inertia of the ball
 p.Theta_omni = 1/2 * p.m_omni * p.r_w^2; % for the actual omniwheels
+%           Vertical Planes
+p.Theta_A = 0.25*p.m_A*(p.r_A^2) + 0.5*p.m_A*(p.h_A^2) + p.m_A*p.l^2;      % kg*m^2, Inertia of the body in vertical planes
+p.Theta_w = 3/2 *cos(p.alpha)^2*(p.Theta_omni + (p.i_Gear^2)*p.Theta_m); % YZ and XZ
 
-% Virtual wheels
-p.Theta_w = 3/2 *cos(deg2rad(p.alpha))^2*(p.Theta_omni + (p.i_Gear^2)*p.Theta_m); % YZ and XZ
-p.Theta_w_xy = 3*(p.Theta_omni + (p.i_Gear^2)*p.Theta_m);   % kg*m^2, Inertia of actuating wheel in xy-plane
+% _______Masses____________
+p.m_w = 3/2 * cos(p.alpha)^2 * p.m_omni;
 
-%p.virt_mass = 2*(2*p.Theta_w + p.Theta_w_xy)/(p.r_w^2);
-p.virt = (2 * p.Theta_w)/((1/3*p.r_w)^2);
+%           XY Plane
+p.Theta_w_xy = 3*(p.Theta_omni + (p.i_Gear^2)*p.Theta_m);   % kg*m^2, Inertia of actuating wheel in XY plane
+p.Theta_A_xy = 0.5 * (p.m_A + p.m_w) * p.r_A^2; % kg*m^2, interia of body in XY plane
+
+
 end
