da2d5f1e2974f91ccd13161afeace19e8ba8dce5
Ryan Stokes <spacebot05@gmail.com>
2026-02-11 16:10:57 -0800

    Renamed variables in params.m to sane ones

--- MATLAB Code/params.m
+++ MATLAB Code/params.m
@@ -4,16 +4,16 @@ function p = params()
 
 % VARIABLE properties %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %_______Masses_______
-p.m_k = 2.29;   %kg, Mass of the Ball
-p.m_A = 9.2;    %kg, Mass of the body
+p.m_ball = 2.29;   %kg, Mass of the Ball
+p.m_body = 9.2;    %kg, Mass of the body
 p.m_omni = 4; %kg, reverse engineered from inertia given
 
 %_______Lengths_______
-p.r_k = .125;   %m, Radius of the ball
-p.r_w = .06;    %m, radius of the OMNIWHEELS, NOT VIRTUAL
-p.r_A = 0.1;     %m, radius of the body (cylinder)
+p.r_ball = .125;   %m, Radius of the ball
+p.r_womni= .06;    %m, radius of the OMNIWHEELS, NOT VIRTUAL
+p.r_body = 0.1;     %m, radius of the body (cylinder)
 
-p.h_A = 0.894;  %m, height of the body (cylinder). Reverse engineered from interia calcs
+p.h_body = 0.894;  %m, height of the body (cylinder). Reverse engineered from interia calcs
 p.l = 0.339;     %m, Height of the center of gravity of body from center of ball
 
 %_______Angle of wheels on ball_______
@@ -25,23 +25,23 @@ p.g = 9.81;     % m/s^2, Gravitational acceleration
 p.i_Gear = 260;  % -, Gear ratio
 
 %_______Inertia Constants_______
-p.Theta_m = 3.33 * 10^-6; % rotor intertia (real small)
+p.Theta_motor_rotor = 3.33 * 10^-6; % rotor intertia (real small)
 
 % CALCULATIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % _________Inertias___________
-p.Theta_k = 2/3 * p.m_k * p.r_k^2; %kgm^2 Inertia of the ball
-p.Theta_omni = 1/2 * p.m_omni * p.r_w^2; % for the actual omniwheels
+p.Theta_ball = 2/3 * p.m_ball * p.r_ball^2; %kgm^2 Inertia of the ball
+p.Theta_omni = 1/2 * p.m_omni * p.r_womni^2; % for the actual omniwheels
 %           Vertical Planes
-p.Theta_A = 0.25*p.m_A*(p.r_A^2) + 0.5*p.m_A*(p.h_A^2) + p.m_A*p.l^2;      % kg*m^2, Inertia of the body in vertical planes
-p.Theta_w = 3/2 *cos(p.alpha)^2*(p.Theta_omni + (p.i_Gear^2)*p.Theta_m); % YZ and XZ
+p.Theta_body_vertical = 0.25*p.m_body*(p.r_body^2) + 0.5*p.m_body*(p.h_body^2) + p.m_body*p.l^2;      % kg*m^2, Inertia of the body in vertical planes
+p.Theta_virt_vertical = 3/2 *cos(p.alpha)^2*(p.Theta_omni + (p.i_Gear^2)*p.Theta_motor_rotor); % YZ and XZ
 
 % _______Masses____________
-p.m_w = 3/2 * cos(p.alpha)^2 * p.m_omni;
+p.m_virt = 3/2 * cos(p.alpha)^2 * p.m_omni;
 
 %           XY Plane
-p.Theta_w_xy = 3*(p.Theta_omni + (p.i_Gear^2)*p.Theta_m);   % kg*m^2, Inertia of actuating wheel in XY plane
-p.Theta_A_xy = 0.5 * (p.m_A + p.m_w) * p.r_A^2; % kg*m^2, interia of body in XY plane
+p.Theta_virt_xy = 3*(p.Theta_omni + (p.i_Gear^2)*p.Theta_motor_rotor);   % kg*m^2, Inertia of actuating wheel in XY plane
+p.Theta_body_xy = 0.5 * (p.m_body + p.m_w) * p.r_body^2; % kg*m^2, interia of body in XY plane
 
 
 end
