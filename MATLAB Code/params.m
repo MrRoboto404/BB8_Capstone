c8b841214ffe9d2fa8b614192ef59f2e3b19e199
Ryan Stokes <spacebot05@gmail.com>
2026-02-11 16:27:36 -0800

    Changed param names and fixed gear ratio

--- MATLAB Code/params.m
+++ MATLAB Code/params.m
@@ -22,7 +22,7 @@ p.beta = deg2rad(0); %rad, taken from +X axis (adds intervals for other location
 
 %_______Misc_______
 p.g = 9.81;     % m/s^2, Gravitational acceleration
-p.i_Gear = 260;  % -, Gear ratio
+p.i_Gear = 26;  % -, Gear ratio
 
 %_______Inertia Constants_______
 p.Theta_motor_rotor = 3.33 * 10^-6; % rotor intertia (real small)
@@ -41,7 +41,7 @@ p.m_virt = 3/2 * cos(p.alpha)^2 * p.m_omni;
 
 %           XY Plane
 p.Theta_virt_xy = 3*(p.Theta_omni + (p.i_Gear^2)*p.Theta_motor_rotor);   % kg*m^2, Inertia of actuating wheel in XY plane
-p.Theta_body_xy = 0.5 * (p.m_body + p.m_w) * p.r_body^2; % kg*m^2, interia of body in XY plane
+p.Theta_body_xy = 0.5 * (p.m_body + p.m_virt) * p.r_body^2; % kg*m^2, interia of body in XY plane
 
 
 end
