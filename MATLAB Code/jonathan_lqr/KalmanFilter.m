60c75de8b359e219bd7c4323135a4c16a375de2c
Jonathan Pham <hieupham0927@gmail.com>
2026-03-10 13:57:42 -0700

    First attempt to solve the moving IMU problem (unfinished)

--- MATLAB Code/jonathan_lqr/KalmanFilter.m
+++ MATLAB Code/jonathan_lqr/KalmanFilter.m
@@ -12,6 +12,7 @@ C = [1, 0, 0, 0;
 % ctrb() and obsv()
 control = rank(ctrb(A,B))
 observe = rank(obsv(A,C))
+
 %%
 % Kalman Filter
 Vd = .001 * eye(4);
@@ -29,3 +30,37 @@ ic_xz = [0; deg2rad(10); 0; 0];  % initial condition
 ic_yz = [0; deg2rad(10); 0; 0];
 ic_xy = [0; 0];
 T_sim = 5; % s
+
+%% Attempt to fix the moving IMU problem
+[A,B,C,D] = get_linearized_matrices_vertical(params());
+% Define physical constants (replace with your actual robot params)
+g = 9.81; 
+r = params().r_ball;  
+L = params().l;
+
+% y_accel = theta + (r/g)*phi_ddot + (L/g)*theta_ddot (small angle approx
+accel_kinematics_A = (r/g) * A(3,:) + (L/g) * A(4,:);
+accel_kinematics_B = (r/g) * B(3,:) + (L/g) * B(4,:);
+
+C(2,:) = C(2,:) + accel_kinematics_A;
+D(2,:) = D(2,:) + accel_kinematics_B; % Feedthrough from motor torque!
+
+% Augment to 5 States to catch the Gyroscope Bias
+% The new state vector is [phi, theta, phidot, thetadot, gyro_bias]^T
+A_aug = [A, zeros(4,1); 
+         zeros(1,4), 0];
+B_aug = [B; 0];
+
+% Augment C and D
+% Add a column to C_orig. 
+% Row 4 is the Gyro. It measures the true theta_dot (State 4) PLUS the bias (State 5)
+C_aug = [C, [0; 0; 0; 1]]; 
+D_aug = D;
+
+% Tune the Filter
+Vd_aug = diag([0.001, 0.001, 0.001, 0.001, 1e-6]); % 5x5 process noise
+Vn = diag([0.001, 0.1, 0.001, 0.001]);             % 4x4 measurement noise
+
+% Calculate Kalman Gain and build Simulink System
+Kf_aug = (lqr(A_aug', C_aug', Vd_aug, Vn))';
+sysKF_aug = ss(A_aug - Kf_aug * C_aug, [B_aug, Kf_aug], eye(5), zeros(5, 1 + size(C_aug, 1)));
