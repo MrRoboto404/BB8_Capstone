52b8cb1eca28922ef9c1fba53bbeddcb98df6562
Ryan Stokes <spacebot05@gmail.com>
2026-05-06 13:03:48 -0700

    Cleans cansniff

--- Arduino Code/velo_control/velo_control.ino
+++ Arduino Code/velo_control/velo_control.ino
@@ -21,12 +21,10 @@ FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can2;
 //______Global Variables______
 unsigned long test_start_time = 0; // Stores the start of the motion
 
-float m1_true_vel;
-float m2_true_vel;
-float m3_true_vel;
-float m1_true_pos;
-float m2_true_pos;
-float m3_true_pos;
+int MOTOR_IDS[3] = {MOTOR_1, MOTOR_2, MOTOR_3};
+float motor_true_vels[3]; // index 0 is motor 1, and so on
+float motor_true_torqs[3];
+float motor_torq_commands[3];
 
 int runcount = 0;
 int t_total = 2;
@@ -141,19 +139,19 @@ void loop() {
             // Print format for Arduino Serial Plotter: "Var1:value Var2:value"
             Serial.print(target_vel_rads_1);
             Serial.print(", ");
-            Serial.print(m1_true_vel * 2.0 * PI);
+            Serial.print(motor_true_vels[0] * 2.0 * PI);
             Serial.print(", ");
             Serial.print(u_k_1, 5);
             Serial.print(", ");
             Serial.print(target_vel_rads_2);
             Serial.print(", ");
-            Serial.print(m2_true_vel * 2.0 * PI);
+            Serial.print(motor_true_vels[1] * 2.0 * PI);
             Serial.print(", ");
             Serial.print(u_k_2, 5);
             Serial.print(", ");
             Serial.print(target_vel_rads_3);
             Serial.print(", ");
-            Serial.print(m3_true_vel * 2.0 * PI);
+            Serial.print(motor_true_vels[2] * 2.0 * PI);
             Serial.print(", ");
             Serial.println(u_k_3, 5);
         }
@@ -358,27 +356,23 @@ void can_sniff(const CAN_message_t &msg) { // global declaration
     10  9  8  7  6  5 |  4  3  2  1  0
           msg ID           cmd_id 
     */
-    float pos, vel, torq;
-    uint8_t node = msg.id >> 5; // shift over to find the node/origin
-    uint8_t cmd  = msg.id & 0x1F; // only look at the cmd_id region by setting those to 1 and the rest 0, then &
-    if (cmd == GET_ENCODER){
-        // Extract data
-        memcpy(&pos, msg.buf, 4);
-        memcpy(&vel, msg.buf + 4, 4);
-
-        // Assign data
-        if(node == 1) m1_true_vel = vel;
-        if(node == 2) m2_true_vel = vel;
-        if(node == 3) m3_true_vel = vel;
+    node = msg.id >> 5; // shift over to find the node/origin
+    cmd  = msg.id & 0x1F; // only look at the cmd_id region by setting those to 1 and the rest 0, then &
+    if (cmd == GET_ENCODER) { // as minimal code as possble
+        motor_true_vels[node - 1] = *reinterpret_cast<const float*>(msg.buf + 4);
+    }
+    else if (cmd == GET_TORQUE) { // as minimal code as possble
+        motor_torq_commands[node - 1] = *reinterpret_cast<const float*>(msg.buf);
+        motor_true_torqs[node - 1] = *reinterpret_cast<const float*>(msg.buf + 4);
     }
 }
 
 // MAIN MOTOR CONTROL LOOP
 void PI_Control_ISR() {
     // Get current velocity and convert rev/s to rad/s
-    float actual_vel_rads_1 = m1_true_vel * 2.0 * PI;
-    float actual_vel_rads_2 = m2_true_vel * 2.0 * PI;
-    float actual_vel_rads_3 = m3_true_vel * 2.0 * PI; 
+    float actual_vel_rads_1 = motor_true_vels[0] * 2.0 * PI;
+    float actual_vel_rads_2 = motor_true_vels[1] * 2.0 * PI;
+    float actual_vel_rads_3 = motor_true_vels[2] * 2.0 * PI; 
 
     // Error signal
     e_k_1 = target_vel_rads_1 - actual_vel_rads_1;
